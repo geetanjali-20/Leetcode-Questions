168ece1b9865dff0f23c9ac1a6817146927500c8
Geetanjali Sharma <66006481+geetanjali-20@users.noreply.github.com>
2023-10-05 14:47:47 +0530

    Time: 7 ms (87.90%), Space: 16.3 MB (9.38%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,47 +1,17 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int sz= nums.size();
-        int n1=-1,n2=-1,c1=0,c2=0,i;
-        for(int i=0;i<sz;i++)
-        {
-            if(n1==nums[i])
-                c1++;
-            else if(nums[i]==n2)
-                c2++;
-            else if(c1==0)
-            {
-                n1=nums[i];
-                c1=1;
-            }
-            else if(c2==0)
-            {
-                n2=nums[i];
-                c2=1;
-                
-            }
-            
-            else
-            {
-                c1--;
-                c2--;
-            }
-        }
-        vector<int> ans;
-        c1=0,c2=0;
-        for(int i=0;i<sz;i++)
-        {
-            if(nums[i]==n1)
-                c1++;
-            else if(nums[i]==n2)
-                c2++;
-            
-        }
-        if(c1>sz/3)
-            ans.push_back(n1);
-        if(c2>sz/3)
-            ans.push_back(n2);
         
+        map<int,int>mpp;
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]]++;
+        }
+        vector<int>ans;
+        int k=nums.size()/3;
+        for(auto it:mpp){
+            if(it.second>k) ans.push_back(it.first);
+            
+        }
         return ans;
-     }    
+    }
 };
